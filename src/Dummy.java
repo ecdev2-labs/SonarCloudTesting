7f5e56a300dc4baac56a3b2bfd286b544b4c35cd
ecdev2-labs <ecdetest10@gmail.com>
2025-07-09 22:47:21 +0530

    Update Dummy.java

--- src/Dummy.java
+++ src/Dummy.java
@@ -1,38 +1,11 @@
-// VulnerableExample.java
+public class Dummy {
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-public class VulnerableExample {
-
-    public static void main(String[] args) {
-        String username = "admin";
-        String password = "admin123"; // Hardcoded credentials
-
-        try {
-            // Insecure SQL statement vulnerable to SQL Injection
-            String userInput = "' OR '1'='1";
-            String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
-
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", username, password);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                System.out.println("User: " + rs.getString("username"));
-            }
-
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Unused method (Sonar will detect this)
-    private void unusedMethod() {
-        System.out.println("This method is never used.");
-    }
+	//int f;
+	
+	public static void main(String ...a)
+	{
+	 
+	  System.out.println("dsfndsavfjdshj");
+	}
+	
 }
