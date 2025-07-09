513510b7149103e6762e80296cd7196a22c35783
ecdev2-labs <ecdetest10@gmail.com>
2025-07-09 22:29:25 +0530

    Update Dummy.java

--- src/Dummy.java
+++ src/Dummy.java
@@ -1,12 +1,12 @@
+// InsecureSQL.java
+import java.sql.*;
 
-public class Dummy {
-
-	//int f;
-	
-	public static void main(String ...a)
-	{
-	 
-	  System.out.println("dsfndsavfjdshj");
-	}
-	
+public class InsecureSQL {
+    public static void main(String[] args) throws Exception {
+        String userInput = "'; DROP TABLE users; --";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "password");
+        Statement stmt = conn.createStatement();
+        String sql = "SELECT * FROM users WHERE username = '" + userInput + "'";  // SQL injection
+        ResultSet rs = stmt.executeQuery(sql);
+    }
 }
